ede111f7f8f1ab9f433388d62fe598c91da8e871
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-09-24 15:12:58 -0700

    add -to-json cmd-line arg to download_all test program

--- download_all/main.go
+++ download_all/main.go
@@ -8,17 +8,23 @@ It downloads all your notes and prints them to stdout.
 */
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 	"time"
 
 	"github.com/kjk/simplenote"
 )
 
+var (
+	toJson   bool
+	maxNotes = -1 // for debugging, 0 or less means "no limit"
+)
+
 func usage() {
-	fmt.Printf("usage: download_all username password\n")
+	fmt.Printf("usage: download_all -to-json username password\n")
 }
 
 func joined(tags []string) string {
@@ -36,27 +42,63 @@ func dumpNote(n *simplenote.Note) {
 	fmt.Printf("Content: %d\n%s\n", len(n.Content), n.Content)
 }
 
+func parseFlags() {
+	flag.BoolVar(&toJson, "to-json", false, "if true, print result as json")
+	flag.Parse()
+}
+
+type Notes struct {
+	NoteInfos []*simplenote.NoteInfo
+	Notes     []*simplenote.Note
+}
+
 func main() {
 	var api *simplenote.Api
+	parseFlags()
+	args := flag.Args()
 	if true {
-		if len(os.Args) != 3 {
+		if len(args) != 2 {
 			usage()
 			return
 		}
-		api = simplenote.New(os.Args[1], os.Args[2])
+		api = simplenote.New(args[0], args[1])
 	} else {
 		api = simplenote.New("foo@bar.com", "password")
 	}
-	notes, err := api.GetNoteList()
+	noteInfos, err := api.GetNoteList()
 	if err != nil {
 		log.Fatalf("api.GetNoteList() returned %q", err)
 	}
-	fmt.Printf("You have %d notes\n", len(notes))
-	for _, ni := range notes {
-		note, err := api.GetNoteLatestVersion(ni.Key)
-		if err != nil {
-			log.Fatalf("api.GetNoteLatestVersion(%q) failed with %q", ni.Key, err)
+	if toJson {
+		var notes []*simplenote.Note
+		for _, ni := range noteInfos {
+			note, err := api.GetNoteLatestVersion(ni.Key)
+			if err != nil {
+				log.Fatalf("api.GetNoteLatestVersion(%q) failed with %q", ni.Key, err)
+			}
+			notes = append(notes, note)
+			maxNotes--
+			if maxNotes == 0 {
+				break
+			}
 		}
-		dumpNote(note)
+		v := &Notes{
+			NoteInfos: noteInfos,
+			Notes:     notes,
+		}
+		jsonStr, err := json.MarshalIndent(v, "", "  ")
+		if err != nil {
+			log.Fatalf("json.MarshalIndent() failed with %q\n", err)
+		}
+		fmt.Println(string(jsonStr))
+	} else {
+		fmt.Printf("You have %d notes\n", len(noteInfos))
+		for _, ni := range noteInfos {
+			note, err := api.GetNoteLatestVersion(ni.Key)
+			if err != nil {
+				log.Fatalf("api.GetNoteLatestVersion(%q) failed with %q", ni.Key, err)
+			}
+			dumpNote(note)
+		}
 	}
 }
