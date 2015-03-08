25ee5bff6e64437402073561b4551adf182c0ee7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-03-08 13:55:24 -0700

    add option to download all versions of notes

--- download_all/main.go
+++ download_all/main.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	maxNotes = -1 // for debugging, 0 or less means "no limit"
+	allVersions = false
 )
 
 func usage() {
@@ -26,12 +26,24 @@ func usage() {
 }
 
 func parseFlags() {
-	//flag.BoolVar(&toJson, "to-json", false, "if true, print result as json")
+	flag.BoolVar(&allVersions, "all-versions", false, "if true, download all versions")
 	flag.Parse()
 }
 
+func printNote(note *simplenote.Note) {
+	d, err := json.MarshalIndent(note, "", "  ")
+	if err != nil {
+		log.Fatalf("json.MarshalIndent() failed with '%s'\n", err)
+	}
+	d = append(d, '\n')
+	_, err = os.Stdout.Write(d)
+	if err != nil {
+		log.Fatalf("os.Stdout.Write() failed with '%s'\n", err)
+	}
+}
+
 func main() {
-	var c *simplenote.Client
+	var client *simplenote.Client
 	parseFlags()
 	args := flag.Args()
 	if true {
@@ -39,28 +51,32 @@ func main() {
 			usage()
 			return
 		}
-		c = simplenote.NewClient(args[0], args[1], args[2])
+		client = simplenote.NewClient(args[0], args[1], args[2])
 	} else {
-		c = simplenote.NewClient("api_key", "foo@bar.com", "password")
+		client = simplenote.NewClient("api_key", "foo@bar.com", "password")
 	}
 
-	notes, err := c.List()
+	notes, err := client.List()
 	if err != nil {
 		log.Fatalf("c.List() failed with '%s'\n", err)
 	}
-	for _, n := range notes {
-		/*n, err := api.GetNote(note.ID, note.V)
-		if err != nil {
-			log.Fatalf("api.GetNote() failed with '%s'\n", err)
-		}*/
-		d, err := json.MarshalIndent(n, "", "  ")
-		if err != nil {
-			log.Fatalf("json.MarshalIndent() failed with '%s'\n", err)
+	for _, note := range notes {
+		printNote(note)
+		if !allVersions {
+			continue
 		}
-		d = append(d, '\n')
-		_, err = os.Stdout.Write(d)
-		if err != nil {
-			log.Fatalf("os.Stdout.Write() failed with '%s'\n", err)
+		ver := note.Version - 1
+		id := note.ID
+		for ver > 0 {
+			n, err := client.GetNote(id, ver)
+			if err != nil {
+				// sometimes older versions don't exist. there doesn't seeme to be
+				// a way to list versions
+				log.Printf("api.GetNote() failed with '%s'\n", err)
+			} else {
+				printNote(n)
+			}
+			ver--
 		}
 	}
 }
